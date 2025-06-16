3b3804895b72d4d733a2e58b18f2bc1bdd8d7c8f
sanidhya2521 <sanu17022007@gmail.com>
2025-06-16 16:02:36 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,12 @@
+c=input("no.1: ")
+d=input("no.2: ")
+c=float(c)
+d=float(d)
+addition=c+d
+substracion=c-d
+multiplicacion=c*d
+division=c/d
+print(addition)
+print(substracion)
+print(multiplicacion)
+print(division) 
---
+++ task 2.py
@@ -0,0 +1,4 @@
+firstname=input("Enter your first name: ")
+lastname=input("Enter your last name: ")
+fullname= firstname+ " " +lastname
+print(f"hello, {fullname}! welcome to the python program")
